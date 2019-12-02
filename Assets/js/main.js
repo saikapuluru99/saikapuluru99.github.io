027d52ec8f92b7e7d24166b989e57ab7e6a2f8e2
saikapuluru99 <saikapuluru@berkeley.edu>
2019-12-02 03:32:38 -0800

    trying out typing text

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,22 +1,36 @@
-const typing = function(txtElement, words, wait=2000) {
+const typing = function(txtElement, words, wait=1000) {
   this.txtElement = txtElement;
   this.words = words;
   this.txt = '';
-  this.Index = 0;
+  this.wordIndex = 0;
   this.wait= parseInt(wait, 10);
   this.type();
   this.reverse = false;
 }
 typing.prototype.type = function() {
-  const wordInd = this.Index % this.words.length;
-  const currTxt = this.words[wordInd];
-  if(this.isDeleting) {
-    this.txt = currTxt.substring(0, this.txt.length - 1);
+  const wordInd = this.wordIndex % this.words.length;
+  const fullTxt = this.words[wordInd];
+  if(this.reverse) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-    this.txt = currTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-  this.txtElement.innerHTML = '<span class="txt">${this.txt}</span>';
-  setTimeout(() => this.type(), 500)
+  console.log(this.txt);
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  let typeSpeed = 100;
+  if(this.reverse) {
+    typeSpeed /= 2;
+  }
+  if(!this.reverse && this.txt==fullTxt){
+    typeSpeed = this.wait;
+    this.reverse = true;
+  } else if (this.reverse && this.txt == '') {
+
+    this.reverse = false;
+    this.wordIndex++;
+    typeSpeed = 100;
+  }
+  setTimeout(() => this.type(), typeSpeed)
 }
 document.addEventListener('DOMContentLoaded', init);
 function init(){
